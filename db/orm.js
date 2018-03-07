fda5ed69193e46a7d63ca5fd6b084bda9c5ec3c2
Jarrett Portnoy <jarrettrportnoy@gmail.com>
2018-03-06 22:14:08 -0800

    fix sequelize connection and move authentication

--- db/orm.js
+++ db/orm.js
@@ -3,110 +3,125 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('edtech', 'root', '', {
   host: 'localhost',
-  port: 6001,
   dialect: 'mysql',
-  pool: {
-    max: 4,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-    handleDisconnects: true,
-    define: {
-      timestamps: true
-    }
-  }
 });
 
-sequelize.authenticate().then(function() {
-  console.log('You are connected to edtech Database on localhost');
-}).catch(function(err) {
-  console.log('Something went wrong, unable to connect to database: ', err);
-});
+// var user = 'root';
+// var password = '';
+// var db = 'edtech';
+
+// var connection = mysql.createConnection({
+
+//   host: 'localhost',
+//   user: user,
+//   password: password,
+//   database: db
+
+// });
+
+// connection.connect(function(err) {
+//   if(err) {
+//     return console.log('There was an error: ', err)
+//   };
+//   console.log('Now connected to ', db);
+// });
 
 //schemas
 
 const School = sequelize.define('school', {
   id: {
-    type: sequelize.INTEGER,
+    type: Sequelize.INTEGER,
     primaryKey: true,
     autoIncrement: true
   },
-  name: sequelize.STRING
+  name: Sequelize.STRING
 },
 {
   underscored: true
 });
 
-const Person = sequelize.define('person', {
+const Teacher = sequelize.define('teacher', {
   id: {
-    type: sequelize.INTEGER,
+    type: Sequelize.INTEGER,
     primaryKey: true,
     autoIncrement: true
   },
-  name: sequelize.STRING,
-  role: sequelize.STRING
+  name: Sequelize.STRING
+});
+
+const Student = sequelize.define('student', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: Sequelize.STRING
 });
 
 const Class = sequelize.define('class', {
   id: {
-    type: sequelize.INTEGER,
+    type: Sequelize.INTEGER,
     primaryKey: true,
     autoIncrement: true
   },
-  name: sequelize.STRING
+  name: Sequelize.STRING
 });
 
 const Lecture = sequelize.define('lecture', {
   id: {
-    type: sequelize.INTEGER,
+    type: Sequelize.INTEGER,
     primaryKey: true,
     autoIncrement: true
   },
-  name: sequelize.STRING
+  name: Sequelize.STRING
 });
 
 const Topic = sequelize.define('topic', {
   id: {
-    type: sequelize.INTEGER,
+    type: Sequelize.INTEGER,
     primaryKey: true,
     autoIncrement: true
   },
-  name: sequelize.STRING
+  name: Sequelize.STRING
 });
 
 const Quiz = sequelize.define('quiz', {
   id: {
-    type: sequelize.INTEGER,
+    type: Sequelize.INTEGER,
     primaryKey: true,
     autoIncrement: true
   },
-  name: sequelize.STRING
+  name: Sequelize.STRING
 });
 
 const Score = sequelize.define('score', {
   id: {
-    type: sequelize.INTEGER,
+    type: Sequelize.INTEGER,
     primaryKey: true,
     autoIncrement: true
   },
-  grade: sequelize.INTEGER
+  grade: Sequelize.INTEGER
 });
 
 const Message = sequelize.define('message', {
   id: {
-    type: sequelize.INTEGER,
+    type: Sequelize.INTEGER,
     primaryKey: true,
     autoIncrement: true
   },
-  text: sequelize.STRING,
+  text: Sequelize.STRING,
   time: {
-    type: sequelize.DATE,
-    defaultValue: sequelize.NOW
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW
   }
 });
 
-Person.belongsTo(School);
-School.hasMany(Person);
+Teacher.belongsTo(School);
+School.hasMany(Teacher);
+Student.belongsTo(School);
+School.hasMany(Student);
+Student.hasMany(Class);
+Class.hasMany(Student);
 Class.belongsTo(School);
 Class.belongsTo(Teacher);
 School.hasMany(Class);
@@ -118,17 +133,27 @@ Lecture.hasMany(Topic);
 Quiz.belongsTo(Topic);
 Topic.hasMany(Quiz);
 Score.belongsTo(Quiz);
-Score.belongsTo(Person);
+Score.belongsTo(Student);
+Student.hasMany(Score);
 Quiz.hasMany(Score);
-Person.hasMany(Score);
-Message.belongsTo(Person);
-Person.hasMany(Message);
+Message.belongsTo(Student);
+Message.belongsTo(Teacher);
+Student.hasMany(Message);
+Teacher.hasMany(Message);
+
+sequelize.authenticate().then(function() {
+  console.log('You are connected to edtech Database on localhost');
+}).catch(function(err) {
+  console.log('Something went wrong, unable to connect to database: ', err);
+});
 
 module.exports.School = School;
-module.exports.Person = Person;
+module.exports.Teacher = Teacher;
+module.exports.Student = Student;
 module.exports.Class = Class;
 module.exports.Lecture = Lecture;
 module.exports.Topic = Topic;
 module.exports.Quiz = Quiz;
 module.exports.Score = Score;
 module.exports.Message = Message;
+module.exports.sequelize = sequelize;
