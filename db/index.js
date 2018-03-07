c55f625bf88688bf9332bd7292dc54d96dc8e071
Jarrett Portnoy <jarrettrportnoy@gmail.com>
2018-03-06 22:10:21 -0800

    move functionality to orm

--- db/index.js
+++ db/index.js
@@ -1,21 +1,29 @@
 const mysql = require('mysql');
+const orm = require('./orm.js');
+// orm.sequelize.sync()
+//   .then(() => {
+//     console.log('connected');
+//   })
+//   .catch(() => {
+//     console.log('u stupid');
+//   });
 
-var user = 'root';
-var password = '';
-var db = 'edtech';
+// var user = 'root';
+// var password = '';
+// var db = 'edtech';
 
-var connection = mysql.createConnection({
+// var connection = mysql.createConnection({
 
-  host: 'localhost',
-  user: user,
-  password: password,
-  database: db
+//   host: 'localhost',
+//   user: user,
+//   password: password,
+//   database: db
 
-});
+// });
 
-connection.connect(function(err) {
-  if(err) {
-    return console.log('There was an error: ', err)
-  };
-  console.log('Now connected to ', db);
-});
+// connection.connect(function(err) {
+//   if(err) {
+//     return console.log('There was an error: ', err)
+//   };
+//   console.log('Now connected to ', db);
+// });
