1d4b0b253e16e3d43b10050e526e1fc0ea07bd8f
Jarrett Portnoy <jarrettrportnoy@gmail.com>
2018-03-07 15:02:46 -0800

    create schools and teachers routes

--- server/router.js
+++ server/router.js
@@ -1,4 +1,10 @@
 const router = require('express').Router();
+const schoolsRouter = require('./routers/schoolsRouter.js');
+const teachersRouter = require('./routers/teachersRouter.js');
+
+
+router.use('/schools', schoolsRouter);
+router.use('/teachers', teachersRouter);
 
 //router.get()
 //router.post()
