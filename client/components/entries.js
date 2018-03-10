5502d439a403d4315f6f174adb94f646afe03949
Jarrett Portnoy <jarrettrportnoy@gmail.com>
2018-03-09 23:53:44 -0800

    add entries component for modifying db

---
+++ client/components/entries.js
@@ -0,0 +1,40 @@
+angular.module('edtechApp')
+.component('entries', {
+  controller: function() {
+
+  },
+
+  template:
+  `
+  <div>
+    <h1>entries!</h1>
+    <div>
+      <h5>Schools</h5>
+    </div>
+    <div>
+      <h5>Teachers</h5>
+    </div>
+    <div>
+      <h5>Students</h5>
+    </div>
+    <div>
+      <h5>Classes</h5>
+    </div>
+    <div>
+      <h5>Lectures</h5>
+    </div>
+    <div>
+      <h5>Topics</h5>
+    </div>
+    <div>
+      <h5>Quizzes</h5>
+    </div>
+    <div>
+      <h5>Quiz Grades</h5>
+    </div>
+    <div>
+      <h5>Add/Remove Students to Classes</h5>
+    </div>
+  </div>
+  `
+})
