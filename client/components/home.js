24c7e1a7dd13b630930d65cacfd4d99c4b456480
Jarrett Portnoy <jarrettrportnoy@gmail.com>
2018-03-09 23:51:45 -0800

    add buttons to switch between entries, classes, and students

--- client/components/home.js
+++ client/components/home.js
@@ -1,18 +1,47 @@
 angular.module('edtechApp')
 .component('home', {
   controller: function() {
+    this.newEntry = false;
+    this.classList = true;
+    this.studentsList = false;
+
+    this.showNewEntry = () => {
+      this.newEntry = true;
+      this.classList = false;
+      this.studentsList = false;
+    };
+
+    this.showClassList = () => {
+      this.classList = true;
+      this.newEntry = false;
+      this.studentsList = false;
+    };
+
+    this.showStudentsList = () => {
+      this.studentsList = true;
+      this.classList = false;
+      this.newEntry = false;
+    };
 
   },
 
   template:
   `
   <div>
-    
-    <div>
+    <button ng-click='$ctrl.showNewEntry()' >Add New Entry</button>
+    <button ng-click='$ctrl.showClassList()' >Show Classes</button>
+    <button ng-click='$ctrl.showStudentsList()' >Show Students</button>
+    <div ng-if='$ctrl.newEntry' >
+      <h1>good job, Jarrett!</h1>
+      <entries>not this</entries>
+    </div>
+    <div ng-if='$ctrl.classList' >
       <h3>List of Classes</h3>
       <classlist></classlist>
     </div>
-    
+    <div ng-if='$ctrl.studentsList' >
+      <h2>this will be students list</h2>
+    </div>
 
   </div>
   `
